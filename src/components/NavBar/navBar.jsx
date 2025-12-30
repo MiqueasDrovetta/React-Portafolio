c7dfa1e720d7d68c6f66d233a635a577bc61f0ba
miqueas.drovetta <miqueas.drovetta@itscordoba.edu.ar>
2025-12-30 22:26:05 +0000

    fix: Use Link component in NavBar for correct routing

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
-
-
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import './navBar.css';
-
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 export const NavBar = () => {
     return (
-
         <Navbar expand="lg" className="bg-body-tertiary navbar-dark bg-dark">
             <Container>
-                <Navbar.Brand href="#home" style={{ color: '#61dafb', fontWeight: 'bold' }}>PORTFOLIO</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" style={{ color: '#61dafb', fontWeight: 'bold' }}>PORTFOLIO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/index">¿Que es Portafolio?</Nav.Link>
+                        <Nav.Link as={Link} to="/index">¿Que es Portafolio?</Nav.Link>
                         <NavDropdown title="Opciones" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/">Home</NavDropdown.Item>
-                            <NavDropdown.Item href="/sobreMi">Sobre mì</NavDropdown.Item>
-                            {/* <NavDropdown.Item href="/card">Card</NavDropdown.Item> */}
-                            <NavDropdown.Item href="/habilidad">Habilidad</NavDropdown.Item>
-                            <NavDropdown.Item href="/tecnologia">Tecnologìa</NavDropdown.Item>
-                            <NavDropdown.Item href="/blog">Blog</NavDropdown.Item>
-                            <NavDropdown.Item href="/usuario">Usuario</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/sobreMi">Sobre mì</NavDropdown.Item>
+                            {/* <NavDropdown.Item as={Link} to="/card">Card</NavDropdown.Item> */}
+                            <NavDropdown.Item as={Link} to="/habilidad">Habilidad</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/tecnologia">Tecnologìa</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/blog">Blog</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/usuario">Usuario</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
